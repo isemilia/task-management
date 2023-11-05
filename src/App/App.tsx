3a34c20949ea2b79db5d863ebe6f3ad7c0e9ef51
Emilia <81705094+emiliaweb@users.noreply.github.com>
2023-11-05 15:26:19 +0600

    Rearrange structure

---
+++ src/App/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const App: FC = () => {
+  return (
+    <>
+      <p>Hello World</p>
+    </>
+  )
+}
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App/App.tsx';
+import theme from './theme/theme.tsx';
+
+import 'normalize.css';
+import './index.scss';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { ThemeProvider } from '@mui/material';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )

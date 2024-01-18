f8cfb81f57bc66d1dce35f893ffc9aa0ef5a9e0c
Emilia <81705094+emiliaweb@users.noreply.github.com>
2024-01-18 22:20:40 +0600

    Set up redux store

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,12 +2,16 @@ import { FC } from 'react';
 
 import { Box } from '@mui/material';
 import AppRouter from './router/router';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const App: FC = () => {
 
   return (
     <Box>
-      <AppRouter />
+      <Provider store={store}>
+        <AppRouter />
+      </Provider>
     </Box>
   )
 }

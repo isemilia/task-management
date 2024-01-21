df197a509d8b2ecf012b3292137c983e7498a9b2
Emilia <81705094+emiliaweb@users.noreply.github.com>
2024-01-21 22:03:46 +0600

    Add todo

--- src/shared/api/base/base-query.ts
+++ src/shared/api/base/base-query.ts
@@ -1,10 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+// TODO: add tags and invalidate on token change
+
 export const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'http://localhost:4000',
     // allow the server to receive the cookies
-    credentials: 'include',
+    credentials: 'include'
   }),
   endpoints: () => ({}),
 })
